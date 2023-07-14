d20a5f8624f0e0ef849879b0080010f38753a84c
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-07-14 14:23:03 -0300

    el usuario queda logeado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/layout/Header';
 import Footer from "./components/layout/Footer";
@@ -6,10 +7,17 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import ProductDetails from './components/product/ProductDetails';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
+import { loadUser } from './actions/userActions';
+import store from "./store"
 
 
 
 function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Router>
       <div className="App">
--- frontend/src/actions/userActions.jsx
+++ frontend/src/actions/userActions.jsx
@@ -6,6 +6,9 @@ import {
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
     CLEAR_ERRORS
 } from "../constants/userConstants"
 
@@ -60,6 +63,27 @@ export const register = (userData) => async(dispatch) => {
         })
     }
 }
+
+// Load user
+export const loadUser = () => async(dispatch) => {
+    try {
+        
+        dispatch({ type : LOAD_USER_REQUEST})
+
+        const {data} = await axios.get("api/v1/me")
+  
+        dispatch({  type: LOAD_USER_SUCCESS,
+                    payload:data.user
+            })
+
+    } catch (error) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
 
 
 // Clear Errors
