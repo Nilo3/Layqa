30dbbc916a2b89fc6cf3ac249c6e0dcc27cdd4ca
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-07-15 12:50:53 -0300

    intentando solucionar bug de dropdown

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,18 +1,16 @@
 import React, { Fragment } from "react";
 import "../../App.css";
-import Search from "./Search";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector} from "react-redux"
+import { useAlert } from "react-alert"
+import { NavLink, Link,   useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
 
-  const searchHandler = (keyword) => {
-    if (keyword.trim()) {
-      navigate(`/search/${keyword}`);
-    } else {
-      navigate("/");
-    }
-  };
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector(state => state.auth)
 
 
 
@@ -30,16 +28,41 @@ const Header = () => {
         </div>
 
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <Link to="Login" className="btn ml-4" id="login_btn">
-            Ingresar
+        <Link to="/cart" style={{ textDecoration: "none "}}>
+        <span id="cart" className="ml-3">Carrito</span>
+          <span className="ml-1" id="cart_count">2</span>
           </Link>
 
-          <span id="cart" className="ml-3">
-            Carrito
-          </span>
-          <span className="ml-1" id="cart_count">
-            2
-          </span>
+          {user ? (
+
+            <div className="ml-4 dropdown d-inline" >
+              <Link to="#!" className="btn btn-secondary dropdown-toggle"
+              type="button" id="dropdownMenuButton" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+
+                <figure className="avatar avatar-nav">
+                  <img 
+                    src={user.avatar && user.avatar.url} 
+                    alt={user && user.name}
+                    className="rounded-circle"
+                  />
+                  
+                </figure>
+                <span>{user && user.name}</span>
+              </Link>
+             
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <Link className="dropdown-item text-danger" to="/">
+                  Logout
+                </Link>
+              </div>
+            </div>
+
+          ): !loading  && <Link to="Login" className="btn ml-4" id="login_btn">Ingresar</Link> }
+          
+          
+
+          
         </div>
       </nav>
     </Fragment>
